085035076f838c06cbb9072326682d759048eec0
Alystair <alystairbaine@gmail.com>
2023-11-01 10:48:37 -0500

    First commit

--- Git Repo Example/ContentView.swift
+++ Git Repo Example/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.square.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
         }
         .padding()
     }
