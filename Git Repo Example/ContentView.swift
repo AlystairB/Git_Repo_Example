d786db2b1582b75d3941331089ab40958deee3fa
Alystair <alystairbaine@gmail.com>
2023-11-01 10:38:53 -0500

    Initial Commit

---
+++ Git Repo Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git Repo Example
+//
+//  Created by acstudent on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git Repo Example/Git_Repo_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Repo_ExampleApp.swift
+//  Git Repo Example
+//
+//  Created by acstudent on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_Repo_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
